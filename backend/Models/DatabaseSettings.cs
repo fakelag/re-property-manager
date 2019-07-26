4b241599c2674de91c211c3721a8612013ebfe98
fakelag <si.harjunpaa@gmail.com>
2019-07-26 13:59:42 +0300

    Add database settings

---
+++ backend/Models/DatabaseSettings.cs
@@ -0,0 +1,20 @@
+namespace backend.Models
+{
+	public class DatabaseCollections
+	{
+		public string Properties { get; set; }
+	}
+	public interface IDatabaseSettings
+	{
+		string ConnectionString { get; set; }
+		string DatabaseName { get; set; }
+
+		DatabaseCollections Collections { get; set; }
+	}
+	public class DatabaseSettings : IDatabaseSettings
+	{
+		public string ConnectionString { get; set; }
+		public string DatabaseName { get; set; }
+		public DatabaseCollections Collections { get; set; }
+	}
+}
