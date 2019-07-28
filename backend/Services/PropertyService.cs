4d6e67bc050c06a64f1fbd031e55dd5c6dca5c51
fakelag <si.harjunpaa@gmail.com>
2019-07-28 17:33:11 +0300

    Change Create to void in PropertyService

--- backend/Services/PropertyService.cs
+++ backend/Services/PropertyService.cs
@@ -18,13 +18,7 @@ namespace backend.Services
 		public Property Get(string id, string userId) => _properties.Find(property => property.Id == id && property.Owner == userId).FirstOrDefault();
         public List<Property> ListByUser(string userId) => _properties.Find(property => property.Owner == userId).ToList();
 		public List<Property> List() => _properties.Find(property => true).ToList();
-
-        public Property Create(Property property)
-        {
-            _properties.InsertOne(property);
-            return property;
-        }
-
+        public void Create(Property property) => _properties.InsertOne(property);
         public void Update(string id, Property propertyIn) => _properties.ReplaceOne(property => property.Id == id, propertyIn);
         public void Remove(Property propertyIn) => _properties.DeleteOne(property => property.Id == propertyIn.Id);
         public void Remove(string id) => _properties.DeleteOne(property => property.Id == id);
