67a36761b59fd01b6dc7dfa024ad0244b9bd1e89
fakelag <si.harjunpaa@gmail.com>
2019-07-28 13:03:14 +0300

    Update BackendSettings.cs to match with appsettings.json

--- backend/Models/BackendSettings.cs
+++ backend/Models/BackendSettings.cs
@@ -3,10 +3,14 @@ namespace backend.Models
 	public interface IBackendSettings
 	{
 		string JwtSecret { get; set; }
+		int JwtLifeTimeMinutes { get; set; }
+		int SessionLifeTimeMinutes { get; set; }
 	}
 
 	public class BackendSettings : IBackendSettings
 	{
 		public string JwtSecret { get; set; }
+		public int JwtLifeTimeMinutes { get; set; }
+		public int SessionLifeTimeMinutes { get; set; }
 	}
 }
