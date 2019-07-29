e0b8cb20def679368a4807d8de0027899cf5a637
fakelag <samuel@RNDs-MacBook-Pro-2.local>
2019-07-29 21:31:20 +0300

    Add InvoiceModel.cs

---
+++ backend/Models/InvoiceModel.cs
@@ -0,0 +1,41 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace backend.Models
+{
+	public class InvoiceModel
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+
+		[BsonElement("amount")]
+		[JsonProperty("amount")]
+		public Decimal Amount { get; set; }
+
+		[JsonProperty("amountPaid")]
+		public Decimal AmountPaid { get; set; }
+
+		[BsonElement("currency")]
+		[JsonProperty("currency")]
+		public string Currency { get; set; }
+
+		[BsonElement("description")]
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		[BsonElement("creationDate")]
+		[JsonProperty("creationDate")]
+		public DateTime CreationDate { get; set; }
+
+		[BsonElement("dueDate")]
+		[JsonProperty("dueDate")]
+		public DateTime DueDate { get; set; }
+
+		[BsonElement("linkedTo")]
+		[JsonProperty("linkedTo")]
+		public ObjectLink LinkedTo { get; set; }
+	}
+}
