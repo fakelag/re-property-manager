bff6a02cf18a4ee85f0a03fb7af561bbda443373
fakelag <samuel@RNDs-MacBook-Pro-2.local>
2019-08-01 19:02:44 +0300

    Add some getter utils to ContractService

--- backend/Services/ContractService.cs
+++ backend/Services/ContractService.cs
@@ -14,7 +14,39 @@ namespace backend.Services
             _properties = service.GetDatabase().GetCollection<Property>(settings.Collections.Properties);
         }
 
-		public RentalContract Get(Property property, string id) => property.Contracts.Find(contract => contract.Id == id);
+		public RentalContract Get(Property property, string contractId) => property.Contracts.Find(contract => contract.Id == contractId);
+
+		public RentalContract Get(string contractId)
+		{
+			var propertyList = _properties.Find(prop => true).ToList();
+
+			foreach (var property in propertyList)
+			{
+				foreach (var contract in property.Contracts)
+				{
+					if (contract.Id == contractId)
+						return contract;
+				}
+			}
+
+			return null;
+		}
+
+		public RentalContract Get(string userId, string id)
+		{
+			var propertyList = _properties.Find(prop => prop.Owner == userId).ToList();
+
+			foreach (var property in propertyList)
+			{
+				foreach (var contract in property.Contracts)
+				{
+					if (contract.Id == id)
+						return contract;
+				}
+			}
+
+			return null;
+		}
 
 		public RentalContract Create(Property property, RentalContract contract)
 		{
@@ -32,7 +64,7 @@ namespace backend.Services
 			var index = property.Contracts.FindIndex(rc => rc.Id == contractId);
 
 			if (index == -1)
-				throw new Exception("Update property not found");
+				throw new Exception("update_property_not_found");
 
 			property.Contracts[index] = contract;
 			property.Contracts[index].Property = property.Id;
