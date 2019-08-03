5e8500191e10b241d6cdde8419ae7de459894528
fakelag <samuel@RNDs-MacBook-Pro-2.local>
2019-08-03 21:24:34 +0300

    Add functioning property list DataTable to <App/>

--- frontend/Views/App.tsx
+++ frontend/Views/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import IProperty from '../interfaces/Property';
+import { Button } from 'primereact/button';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 const App = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [properties, setProperties] = useState<IProperty[]>([]);
+
+	useEffect(() => {
+		const fetchProperties = async () => {
+			try {
+				const response = await axios.get<IProperty[]>('/api/property');
+
+				if (response.status === 200 && response.data)
+					setProperties(response.data);
+			} catch (err) {
+				console.error('Login connection error: ', err);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchProperties();
+	}, []);
+
 	return (<>
-		App
-		<a href="/api/user/logout">Log Out</a>
+		{isLoading
+			&& <ProgressSpinner />
+			|| <DataTable
+				value={properties}
+			>
+				<Column field="address" header="Address" />
+				<Column field="city" header="City" />
+				<Column field="apartmentType" header="Type" />
+			</DataTable>
+		}
+		<Button label="Log Out" icon="pi pi-sign-out" onClick={(e) => window.location.href = "/api/user/logout"} />
 	</>);
 };
 
