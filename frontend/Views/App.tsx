c9f7653884925d1c56a583c186024c123583d1d7
fakelag <samuel@RNDs-MacBook-Pro-2.local>
2019-08-04 20:09:50 +0300

    Refactored <App/>

--- frontend/Views/App.tsx
+++ frontend/Views/App.tsx
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import IProperty from '../interfaces/Property';
-import { Button } from 'primereact/button';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { ProgressSpinner } from 'primereact/progressspinner';
+import React, { useState } from 'react';
+import Header from '../Components/Header';
+import PropertyList from '../Components/PropertyList';
 
 const App = () => {
-	const [isLoading, setIsLoading] = useState<boolean>(true);
-	const [properties, setProperties] = useState<IProperty[]>([]);
-
-	useEffect(() => {
-		const fetchProperties = async () => {
-			try {
-				const response = await axios.get<IProperty[]>('/api/property');
-
-				if (response.status === 200 && response.data)
-					setProperties(response.data);
-			} catch (err) {
-				console.error('Login connection error: ', err);
-			} finally {
-				setIsLoading(false);
-			}
-		};
-
-		fetchProperties();
-	}, []);
-
+	const [searchString, setSearchString] = useState<string>('');
 	return (<>
-		{isLoading
-			&& <ProgressSpinner />
-			|| <DataTable
-				value={properties}
-			>
-				<Column field="address" header="Address" />
-				<Column field="city" header="City" />
-				<Column field="apartmentType" header="Type" />
-			</DataTable>
-		}
-		<Button label="Log Out" icon="pi pi-sign-out" onClick={(e) => window.location.href = "/api/user/logout"} />
+		<Header
+			searchPlaceholder="Search properties..."
+			onSearch={(filterString: string) => setSearchString(filterString)}
+		/>
+		<section className="Dashboard">
+			<PropertyList filterByAddress={searchString} />
+		</section>
 	</>);
 };
 
