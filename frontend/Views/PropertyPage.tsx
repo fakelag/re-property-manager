3a0e6a59102d4dc2847e76aaa9099efcbeb12789
fakelag <samuel@RNDs-MacBook-Pro-2.local>
2019-08-06 21:00:00 +0300

    Add property deletion to PropertyPage

--- frontend/Views/PropertyPage.tsx
+++ frontend/Views/PropertyPage.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { match as MatchParams } from 'react-router';
-// import IProperty from '../interfaces/Property';
+import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Dialog } from 'primereact/dialog';
+import router from '../router';
 
 const PropertyPage = ({ match }: { match: MatchParams<{ propertyId: string }> }) => {
-	console.log(match.params.propertyId);
+	const [deleteDialog, setDeleteDialog] = useState(false);
+
+	const deleteProperty = async () => {
+		try {
+			const response = await axios.delete(`/api/property/${match.params.propertyId}`);
+
+			if (response.status === 200) {
+				setDeleteDialog(false);
+				router.push('/');
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
 	return (<div className="PropertyPage">
 		<DataTable
 			value={[]}
@@ -15,6 +32,28 @@ const PropertyPage = ({ match }: { match: MatchParams<{ propertyId: string }> })
 		>
 			<Column field="id" header="Id" />
 		</DataTable>
+		<Dialog
+			header="Delete the property"
+			visible={deleteDialog}
+			style={{ width: '20rem' }}
+			footer={<>
+				<Button label="Delete" icon="pi pi-check" onClick={deleteProperty}
+					className="p-button-danger"/>
+				<Button label="Cancel" icon="pi pi-times" onClick={() => setDeleteDialog(false)}
+					className="p-button-secondary" />
+			</>}
+			onHide={() => setDeleteDialog(false)}
+			maximizable
+		>
+			Do you want to delete this property permanently?
+		</Dialog>
+		<Button
+			className="p-button-danger"
+			label="Delete property"
+			icon="pi pi-trash"
+			iconPos="left"
+			onClick={() => setDeleteDialog(true)}
+		/>
 	</div>);
 };
 
