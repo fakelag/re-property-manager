bbf843ab99c97be8d75954fe0c083d87d866d543
fakelag <35497506+fakelag@users.noreply.github.com>
2019-08-10 19:40:48 +0300

    Add transactions integration into InvoiceService (precalculate amountPaid)

--- backend/Services/InvoiceService.cs
+++ backend/Services/InvoiceService.cs
@@ -8,17 +8,25 @@ namespace backend.Services
 	public class InvoiceService
 	{
 		private readonly IMongoCollection<Invoice> _invoices;
+		private readonly TransactionService _transactionService;
 		private readonly ContractService _contractService;
 
-		public InvoiceService(IDatabaseSettings settings, MongoService mongoService, ContractService contractService)
+		public InvoiceService(IDatabaseSettings settings, MongoService mongoService,
+			ContractService contractService, TransactionService transactionService)
 		{
 			_invoices = mongoService.GetDatabase().GetCollection<Invoice>(settings.Collections.Invoices);
 			_contractService = contractService;
+			_transactionService = transactionService;
 		}
 
-		public Invoice Get(string id) => _invoices.Find(inv => inv.Id == id).FirstOrDefault();
-		public Invoice Get(string id, string userId) => _invoices.Find(inv => inv.Id == id && inv.Owner == userId).FirstOrDefault();
-		public List<Invoice> ListByUser(string userId) => _invoices.Find(inv => inv.Owner == userId).ToList();
+		public Invoice Get(string id) =>
+			UpdateAmountPaid(_invoices.Find(inv => inv.Id == id).FirstOrDefault(), _transactionService.List());
+
+		public Invoice Get(string id, string userId) =>
+			UpdateAmountPaid(_invoices.Find(inv => inv.Id == id && inv.Owner == userId).FirstOrDefault(), _transactionService.ListByUser(userId));
+
+		public List<Invoice> ListByUser(string userId) =>
+			UpdateAmountsPaid(_invoices.Find(inv => inv.Owner == userId).ToList(), _transactionService.ListByUser(userId));
 
 		public Invoice Create(string ownerId, int amount,
 			string currency, DateTime dueDate, string description = "",
@@ -59,12 +67,36 @@ namespace backend.Services
 			invoice.Id = inv.Id;
 			_invoices.ReplaceOne(nv => nv.Id == invoice.Id, invoice);
 
-			return invoice;
+			return UpdateAmountPaid(invoice, _transactionService.ListByUser(invoice.Owner));
 		}
 
         public void Remove(string id)
 		{
 			_invoices.FindOneAndDelete(inv => inv.Id == id);
+		}
+
+		private Invoice UpdateAmountPaid(Invoice invoice, List<Transaction> transactions) {
+			if (invoice == null)
+				return null;
+
+			foreach (Transaction tr in transactions)
+			{
+				foreach (TransactionPart part in tr.Parts)
+				{
+					if (part.Invoice != invoice.Id)
+						continue;
+
+					invoice.AmountPaid += part.Amount;
+				}
+			}
+
+			return invoice;
+		}
+		
+		private List<Invoice> UpdateAmountsPaid(List<Invoice> invoices, List<Transaction> transactions) {
+			return invoices.ConvertAll((invoice) => {
+				return UpdateAmountPaid(invoice, transactions);
+			});
 		}
 	}
 }
