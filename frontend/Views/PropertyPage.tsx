f4452d7a766346682d2edf3560d2f16b73c676c7
fakelag <35497506+fakelag@users.noreply.github.com>
2019-08-10 15:39:50 +0300

    Add "Add Contract" button to PropertyPage

--- frontend/Views/PropertyPage.tsx
+++ frontend/Views/PropertyPage.tsx
@@ -35,38 +35,48 @@ const PropertyPage = ({ match }: { match: MatchParams<{ propertyId: string }> })
 	return (<div className="PropertyPage">
 		{isLoading ? <ProgressSpinner /> :
 			property ? <>
-				<DataTable
-					value={property.contracts}
-					selectionMode="single"
-					onRowSelect={(e: { originalEvent: Event; data: any; type: string; }) =>
-						console.log('selected contract: ', e.data)}
-				>
-					<Column field="id" header="Id" />
-					<Column field="participant.fullName" header="Participant Name" />
-					<Column field="participant.email" header="Participant Email" />
-				</DataTable>
-				<Dialog
-					header="Delete the property"
-					visible={deleteDialog}
-					style={{ width: '20rem' }}
-					footer={<>
-						<Button label="Delete" icon="pi pi-check" onClick={deleteProperty}
-							className="p-button-danger"/>
-						<Button label="Cancel" icon="pi pi-times" onClick={() => setDeleteDialog(false)}
-							className="p-button-secondary" />
-					</>}
-					onHide={() => setDeleteDialog(false)}
-					maximizable
-				>
-					Do you want to delete this property permanently?
-				</Dialog>
-				<Button
-					className="p-button-danger"
-					label="Delete property"
-					icon="pi pi-trash"
-					iconPos="left"
-					onClick={() => setDeleteDialog(true)}
-				/>
+				<section>
+					<DataTable
+						value={property.contracts}
+						selectionMode="single"
+						onRowSelect={(e: { originalEvent: Event; data: IContract; type: string; }) =>
+							router.push(`/contractsettings/${property.id}/${e.data.id}`)}
+					>
+						<Column field="id" header="Id" />
+						<Column field="participant.fullName" header="Participant Name" />
+						<Column field="participant.email" header="Participant Email" />
+					</DataTable>
+					<Dialog
+						header="Delete the property"
+						visible={deleteDialog}
+						style={{ width: '20rem' }}
+						footer={<>
+							<Button label="Delete" icon="pi pi-check" onClick={deleteProperty}
+								className="p-button-danger"/>
+							<Button label="Cancel" icon="pi pi-times" onClick={() => setDeleteDialog(false)}
+								className="p-button-secondary" />
+						</>}
+						onHide={() => setDeleteDialog(false)}
+						maximizable
+					>
+						Do you want to delete this property permanently?
+					</Dialog>
+				</section>
+				<section>
+					<Button
+						className="p-button-info"
+						label="Add Contract"
+						iconPos="left"
+						onClick={() => router.push(`/contractsettings/${property.id}`)}
+					/>
+					<Button
+						className="p-button-danger"
+						label="Delete property"
+						icon="pi pi-trash"
+						iconPos="left"
+						onClick={() => setDeleteDialog(true)}
+					/>
+				</section>
 			</> : <>404 Not Found</>
 		}
 	</div>);
